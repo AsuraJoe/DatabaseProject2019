bbbfadc3b13c35d8de598ef7a4f0b9df18088224
unknown <ygeng1@students.towson.edu>
2019-11-25 23:38:52 -0500

    increment and drop

--- create.sql
+++ create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE entity (
-    entity_id int not null,
+    entity_id int not null AUTO_INCREMENT,
     primary key (entity_id)
 );
 create table entity_instance (
-    instance_number int not null,
+    instance_number int not null AUTO_INCREMENT,
     primary key (instance_number),
     entity_id int,
     foreign key (entity_id) references entity(entity_id),
@@ -12,26 +12,27 @@ create table entity_instance (
     z float
 );
 create table item (
-    item_id int not null,
+    item_id int not null AUTO_INCREMENT,
+    item_name varchar(40) not null,
     primary key (item_id),
     max_stack int,
     placeable boolean
 );
 create table item_instance(
-    instance_number int not null,
+    instance_number int not null AUTO_INCREMENT,
     primary key (instance_number),
     stack int,
     item_id int,
     foreign key (item_id) references item (item_id)
 );
 create table block(
-    block_id int not null,
+    block_id int not null AUTO_INCREMENT,
     primary key (block_id),
     transparent boolean,
     gravity boolean
 );
 create table block_instance (
-    instance_number int not null,
+    instance_number int not null AUTO_INCREMENT,
     primary key (instance_number),
     breaking_stage int,
     x int,
@@ -63,19 +64,19 @@ create table structure(
     foreign key (biome) references biome(name)
 );
 create table enchantment(
-    enchantment_id int unique not null,
+    enchantment_id int unique not null AUTO_INCREMENT,
     primary key (enchantment_id),
     max_level int
 );
 create table inventory(
-    entity_instance_number int,
+    entity_instance_number int AUTO_INCREMENT,
     foreign key (entity_instance_number) references entity_instance (instance_number),
     slot int,
     item_instance_number int,
     foreign key (item_instance_number) references item_instance (instance_number)
 );
 create table player(
-    instance_number int unique not null,
+    instance_number int unique not null AUTO_INCREMENT,
     foreign key (instance_number) references entity_instance(instance_number),
     username varchar(45) unique not null,
     health int,
@@ -83,7 +84,7 @@ create table player(
     air int
 );
 create table mob(
-    instance_number int  not null,
+    instance_number int  not null AUTO_INCREMENT,
     foreign key (instance_number) references entity_instance(instance_number),
     mob_type varchar(45),
     health int,
@@ -91,36 +92,36 @@ create table mob(
     constraint UC_mob unique (instance_number)
 );
 create table armor_stand(
-    instance_number int not null,
+    instance_number int not null AUTO_INCREMENT,
     foreign key (instance_number) references entity_instance(instance_number),
     constraint UC_stand unique(instance_number)
 );
 create table minecart(
-    instance_number int not null,
+    instance_number int not null AUTO_INCREMENT,
     foreign key (instance_number) references entity_instance(instance_number),
     damage int,
      constraint UC_cart unique(instance_number)
 );
 create table block_drops(
-    block_id int not null,
+    block_id int not null AUTO_INCREMENT,
     foreign key (block_id) references block(block_id),
     item_id int not null,
     foreign key (item_id) references item(item_id)
 );
 create table creates_block(
-    item_id int not null,
+    item_id int not null AUTO_INCREMENT,
     foreign key (item_id) references item(item_id),
     block_id int not null,
     foreign key (block_id) references block(block_id)
 );
 create table creates_entity(
-    item_id int not null,
+    item_id int not null AUTO_INCREMENT,
     foreign key (item_id) references item(item_id),
     entity_id int,
     foreign key (entity_id) references entity(entity_id)
 );
 create table entity_drops(
-    entity_id int,
+    entity_id int AUTO_INCREMENT,
     foreign key (entity_id) references entity(entity_id),
     item_id int,
     foreign key (item_id) references item(item_id)
--- insertion.sql
+++ insertion.sql
@@ -1,1 +1,12 @@
-#insert code here
+set FOREIGN_KEY_CHECKS =0;
+/*insert item*/
+insert INTO item (item_name, max_stack) Values('iron Sword',1);
+insert INTO item (item_name, max_stack) Values('Wooden Sword',1);
+insert INTO item (item_name, max_stack) Values('Golden Sword',1);
+insert INTO item (item_name, max_stack) Values('Diamond Sword',1);
+insert INTO item (item_name, max_stack) Values('Diamond Axe',1);
+insert INTO item (item_name, max_stack) Values('Golden Axe',1);
+insert INTO item (item_name, max_stack) Values('iron Axe',1);
+insert INTO item (item_name, max_stack) Values('Wooden Axe',1);
+insert INTO item (item_name, max_stack) Values('Diamond Hoe',1);
+set FOREIGN_KEY_CHECKS =1;
