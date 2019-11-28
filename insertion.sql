426d9bf60dbd3d5654ec9fd6d1642dbaa75f4a48
YuanyeG <55464933+YuanyeG@users.noreply.github.com>
2019-11-27 21:50:39 -0500

    Update insertion.sql

--- insertion.sql
+++ insertion.sql
@@ -98,20 +98,119 @@ insert INTO block(block_name, transparent, gravity) Values('Lapis Lazuli Ore', f
 insert INTO block_instance(breaking_stage,x,y,z) Values(1,2,3,4);
 
 /*chunk*/
-insert INTO chunk(lower_x,lower_y,lower_z,upper_x,upper_y,upper_z)values(4,5,6,1,2,3);
+insert INTO chunk(lower_x,lower_y,lower_z,upper_x,upper_y,upper_z)values(1,2,3,4,5,6);
 
 /*biome*/
+insert INTO biome(name) values ('Ocean');
+insert INTO biome(name) values ('Deep Ocean');
+insert INTO biome(name) values ('Frozen Ocean');
+insert INTO biome(name) values ('Cold Ocean');
+insert INTO biome(name) values ('River');
+insert INTO biome(name) values ('Frozen River');
+insert INTO biome(name) values ('Beach');
+insert INTO biome(name) values ('Forest');
+insert INTO biome(name) values ('Wooded Hills');
+insert INTO biome(name) values ('Flower Forest');
+insert INTO biome(name) values ('Birch Forest');
+insert INTO biome(name) values ('Birch Forest Hills');
+insert INTO biome(name) values ('Dark Forest');
+insert INTO biome(name) values ('Dark Forest Hills');
+insert INTO biome(name) values ('Jungle');
+insert INTO biome(name) values ('Jungle Hills');
+insert INTO biome(name) values ('Jungle Edge');
+insert INTO biome(name) values ('Bamboo Jungle');
+insert INTO biome(name) values ('Taiga');
+insert INTO biome(name) values ('Taiga Mountains');
+insert INTO biome(name) values ('Swamp');
+insert INTO biome(name) values ('Savanna');
+insert INTO biome(name) values ('Plains');
+insert INTO biome(name) values ('Sunflower Plains');
+insert INTO biome(name) values ('Desert');
+insert INTO biome(name) values ('Desert Hills');
+insert INTO biome(name) values ('Desert Lakes');
 insert INTO biome(name) values ('Snowy Tundra');
+insert INTO biome(name) values ('Snowy Mountains');
+insert INTO biome(name) values ('Mountains');
+insert INTO biome(name) values ('Wooded Mountains');
+insert INTO biome(name) values ('Gravelly Mountains');
+insert INTO biome(name) values ('Mountain Edge');
+insert INTO biome(name) values ('Badlands');
 
 /*biome_blocks*/
+insert INTO biome_blocks(name, block_id) Values ('ocean',0);
+insert INTO biome_blocks(name, block_id) Values ('Deep Ocean',24);
+insert INTO biome_blocks(name, block_id) Values ('Frozen Ocean',10);
+insert INTO biome_blocks(name, block_id) Values ('Cold Ocean',46);
+insert INTO biome_blocks(name, block_id) Values ('River',7);
+insert INTO biome_blocks(name, block_id) Values ('Frozen River',11);
+insert INTO biome_blocks(name, block_id) Values ('Beach',16);
+insert INTO biome_blocks(name, block_id) Values ('Forest',4);
+insert INTO biome_blocks(name, block_id) Values ('Wooded Hills',18);
+insert INTO biome_blocks(name, block_id) Values ('Flower Forest',132);
+insert INTO biome_blocks(name, block_id) Values ('Birch Forest',27);
+insert INTO biome_blocks(name, block_id) Values ('Birch Forest Hills',28);
+insert INTO biome_blocks(name, block_id) Values ('Dark Forest',29);
+insert INTO biome_blocks(name, block_id) Values ('Dark Forest Hills',157);
+insert INTO biome_blocks(name, block_id) Values ('Jungle',21);
+insert INTO biome_blocks(name, block_id) Values ('Jungle Hills',22);
+insert INTO biome_blocks(name, block_id) Values ('Jungle Edge',23);
+insert INTO biome_blocks(name, block_id) Values ('Bamboo Jungle',168);
+insert INTO biome_blocks(name, block_id) Values ('Taiga',5);
+insert INTO biome_blocks(name, block_id) Values ('Taiga Mountains',19);
+insert INTO biome_blocks(name, block_id) Values ('Swamp',6);
+insert INTO biome_blocks(name, block_id) Values ('Savanna',35);
+insert INTO biome_blocks(name, block_id) Values ('Plains',1);
+insert INTO biome_blocks(name, block_id) Values ('Sunflower Plains',129);
+insert INTO biome_blocks(name, block_id) Values ('Desert',2);
+insert INTO biome_blocks(name, block_id) Values ('Desert Hills',17);
+insert INTO biome_blocks(name, block_id) Values ('Desert Lakes',130);
 insert INTO biome_blocks(name, block_id) Values ('Snowy Tundra',12);
+insert INTO biome_blocks(name, block_id) Values ('Snowy Mountains',13);
+insert INTO biome_blocks(name, block_id) Values ('Mountains',3);
+insert INTO biome_blocks(name, block_id) Values ('Wooded Mountains',34);
+insert INTO biome_blocks(name, block_id) Values ('Gravelly Mountains',131);
+insert INTO biome_blocks(name, block_id) Values ('Mountain Edge',20);
+insert INTO biome_blocks(name, block_id) Values ('Badlands',37);
 
 /*structure*/
 insert INTO structure(name, biome)values ('Mineshaft','Overworld');
+insert INTO structure(name, biome)values ('Stronghold','Any');
+insert INTO structure(name, biome)values ('Fossil','Desert');
+insert INTO structure(name, biome)values ('Buried treasure','Beach, Snowy Beach, Ocean, Stone Shore');
+insert INTO structure(name, biome)values ('Ocean ruin','Ocean, Beach');
+insert INTO structure(name, biome)values ('Shipwreck','Ocean, Beach');
+insert INTO structure(name, biome)values ('Ocean Monument','Deep Ocean');
+insert INTO structure(name, biome)values ('Desert pyramid','Desert');
+insert INTO structure(name, biome)values ('Igloo','Snowy Tundra');
+insert INTO structure(name, biome)values ('Jungle pyramid','Jungle');
+insert INTO structure(name, biome)values ('Village','Plains, Desert, Taipa');
+insert INTO structure(name, biome)values ('Swamp hut','Swamp');
+insert INTO structure(name, biome)values ('Woodland mansion','Deep Forest');
+insert INTO structure(name, biome)values ('Dungeon','Any');
+insert INTO structure(name, biome)values ('Fortress','The Nether');
 
 /*insert enchantment*/
 /*max level cant be int '1' as 'I' */
 insert INTO enchantment(enchantment_id,max_level)values('Aqua Affinity', '1');
+insert INTO enchantment(enchantment_id,max_level)values('Bane of Arthropods', '4');
+insert INTO enchantment(enchantment_id,max_level)values('Blast Protection', '5');
+insert INTO enchantment(enchantment_id,max_level)values('Channeling', '1');
+insert INTO enchantment(enchantment_id,max_level)values('Curse of Binding', '1');
+insert INTO enchantment(enchantment_id,max_level)values('Curse of Vanishing', '1');
+insert INTO enchantment(enchantment_id,max_level)values('Depth Strider', '3');
+insert INTO enchantment(enchantment_id,max_level)values('Efficiency', '4');
+insert INTO enchantment(enchantment_id,max_level)values('Feather Falling', '5');
+insert INTO enchantment(enchantment_id,max_level)values('Fire Aspect', '2');
+insert INTO enchantment(enchantment_id,max_level)values('Fire Protection', '5');
+insert INTO enchantment(enchantment_id,max_level)values('Flame', '1');
+insert INTO enchantment(enchantment_id,max_level)values('Fortune', '3');
+insert INTO enchantment(enchantment_id,max_level)values('Frost Walker', '2');
+insert INTO enchantment(enchantment_id,max_level)values('Impaling', '4');
+insert INTO enchantment(enchantment_id,max_level)values('Infinity', '1');
+insert INTO enchantment(enchantment_id,max_level)values('Knockback', '2');
+insert INTO enchantment(enchantment_id,max_level)values('Looting', '3');
+insert INTO enchantment(enchantment_id,max_level)values('Loyalty', '3');
+insert INTO enchantment(enchantment_id,max_level)values('Mending', '1');
 
 /*inventory*/
 insert INTO inventory(slot, item_instance_number) values (1, 1);
@@ -120,7 +219,26 @@ insert INTO inventory(slot, item_instance_number) values (1, 1);
 insert INTO player(username, health, hunger, air) Values ('Steve', 20,9,12);
 
 /*insert mob*/
+/*idk what air is in here so all 12*/
 insert INTO mob(mob_name, mob_type, health, air) Values('Cave spider', 'Monsters',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Chicken', 'Peaceful',4,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Cow', 'Peaceful',10,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Cat', 'Peaceful',10,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Horse', 'Monsters',15,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Pufferfish', 'Defensive',3,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Wolf', 'Animals',8,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Panda', 'Animals',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Enderman', 'Monsters',40,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Spider', 'Monsters',16,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Zombie Pigman', 'Monsters',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Creeper', 'Hostile',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Zombie', 'Hostile',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Zombie Villager', 'Hostile',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Slime', 'Hostile',4,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('NPC', 'Education Edition exclusive mobs',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Blaze', 'Hostile',20,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Guardian', 'Hostile',30,12);
+insert INTO mob(mob_name, mob_type, health, air) Values('Ghast', 'Hostile',10,12);
 
 /*armor_stand*/
 insert into armor_stand()values();
@@ -142,4 +260,4 @@ insert into structure_made_of(name, block_id, x,y,z)values('dont know this one',
 
 set FOREIGN_KEY_CHECKS =1;
 
-Select * from block_drops;
+Select * from item;
